5330f4f10830e7038915df5cf72169d6765d46f0
Wivers Server <matthieuganet@gmail.com>
2026-01-14 19:00:38 +0000

    setup symfony backend, and create api_ping route. (react front communicate with symfony back without error)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const r = await fetch("/api/ping");
+console.log(await r.json());
 function App() {
   const [count, setCount] = useState(0)
 
