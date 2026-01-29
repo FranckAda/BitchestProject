5bf50b79bb152dca52f38916552850cbae096c84
Franck <162502379+Franckad1@users.noreply.github.com>
2026-01-29 17:51:19 +0100

    Update Controllers

--- backend/src/Controller/ClientController.php
+++ backend/src/Controller/ClientController.php
@@ -3,16 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\UserType;
 use App\Repository\UserRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/user')]
-final class UserController extends AbstractController
+#[Route('/client')]
+final class ClientController extends AbstractController
 {
     #[Route(name: 'app_client_index', methods: ['GET'])]
     public function index(UserRepository $userRepository): Response
