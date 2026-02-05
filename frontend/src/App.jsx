52a80bc9871125429cfaaecc53439d89cc578ca0
Franck <162502379+Franckad1@users.noreply.github.com>
2026-02-05 17:27:12 +0100

    Config for api on windows and a form to test api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,31 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Login from './pages/Login';
-import './App.css'
-
-const r = await fetch("/api/health");
-console.log(await r.json());
-
+import { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Login from "./pages/Login";
+import "./App.css";
+import EditForm from "./Components/EditForm";
 
 export default function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    fetch("/api/health") // ✅ passe par le proxy Vite
+      .then((r) => r.json())
+      .then((data) => console.log("health:", data))
+      .catch((err) => console.error("health error:", err));
+  }, []);
 
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/edit" element={<EditForm userId={1} />} />
       </Routes>
     </Router>
-  )
+  );
 }
